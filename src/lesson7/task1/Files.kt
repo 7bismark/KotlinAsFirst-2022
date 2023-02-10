ce89250391c34362cd4dffc16843ea556d717389
Arsen Guseinov <arsenbaxmax@gmail.com>
2023-02-10 13:54:37 +0300

    Уроки 5 6 7

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -264,31 +264,32 @@ fun transliterate(inputName: String, dictionary: Map<Char, String>, outputName: 
  * Обратите внимание: данная функция не имеет возвращаемого значения
  */
 fun chooseLongestChaoticWord(inputName: String, outputName: String)
-    {try {
-            val set = mutableSetOf<String>()
-            for (line in File(inputName).readLines()) {
-                set.add(line)
-            }
-            val max = set.map { it.length }.max()
-            val resultSet = set
-                .filter { checkIsReplayNot(it) }
-                .also {
-                    it
-                }
-                .filter { it.length == max }
-            var resultString = resultSet.first()
-            if (resultSet.size > 1) {
-                resultString = resultSet.reduce { sum, newLine -> "$sum, $newLine" }
-            }
-            val writer = File(outputName).bufferedWriter()
-            writer.write(resultString)
-            writer.close()
-        } catch (e: Throwable) {
-            val writer = File(outputName).bufferedWriter()
-            writer.write("")
-            writer.close()
+{
+    try {
+        val inputList = mutableListOf<String>()
+        for (line in File(inputName).readLines()) {
+            inputList.add(line)
         }
+        val max = inputList.map { it.length }.max()
+        val resultSet = inputList
+            .filter { checkIsReplayNot(it) }
+            .also {
+                it
+            }
+            .filter { it.length == max }
+        var resultString = resultSet.first()
+        if (resultSet.size > 1) {
+            resultString = resultSet.reduce { sum, newLine -> "$sum, $newLine" }
+        }
+        val writer = File(outputName).bufferedWriter()
+        writer.write(resultString)
+        writer.close()
+    } catch (e: Throwable) {
+        val writer = File(outputName).bufferedWriter()
+        writer.write("")
+        writer.close()
     }
+}
 
 private fun checkIsReplayNot(string: String): Boolean {
     return string.length == string.split("").toSet().filter { it.isNotBlank() }.size
